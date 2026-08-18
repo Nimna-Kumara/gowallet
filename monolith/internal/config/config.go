a5b7dfa291bb617a3e22551addb78a8f7d3b7088
Nimna-Kumara <nimnaanjana2001@gmail.com>
2026-08-18 17:11:24 +0530

    added comments in internal/config/config.go line: 16, 21

--- monolith/internal/config/config.go
+++ monolith/internal/config/config.go
@@ -8,6 +8,7 @@ import (
 )
 
 type Config struct {
+	// DSN = Data Source Name
 	DBDSN string
 }
 
@@ -17,6 +18,7 @@ func LoadConfig() *Config {
 		log.Println("Warning: .env file not found, using environment variables, err: %w", err)
 	}
 
+	// gowallet_user:123456@tcp(localhost:3306)/gowallet?parseTime=true
 	dsn := os.Getenv("DB_USER") + ":" +
 		os.Getenv("DB_PASSWORD") + "@tcp(" +
 		os.Getenv("DB_HOST") + ":" +
